28c2c74d2927454977339fdafd9a73e1b3671aa4
Cara Quenia <lacaraquenia@yahoo.com>
2021-05-03 10:44:37 +0200

    Ajoute les annotations Lombok a l'entity Requete. Fonctionne bien

--- src/main/java/fr/nsa/jdd/requete/model/Requete.java
+++ src/main/java/fr/nsa/jdd/requete/model/Requete.java
@@ -1,48 +1,35 @@
 package fr.nsa.jdd.requete.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
 @Entity
 @Table (name = "requete")
 public class Requete {
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
 	protected Long id;
 	
+	@Getter
+	@Setter
 	protected String nom_requete;
 	
+	@Getter
+	@Setter
 	protected String desc_requete;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNom_requete() {
-		return nom_requete;
-	}
-
-	public void setNom_requete(String nom_requete) {
-		this.nom_requete = nom_requete;
-	}
-
-	public String getDesc_requete() {
-		return desc_requete;
-	}
-
-	public void setDesc_requete(String desc_requete) {
-		this.desc_requete = desc_requete;
-	}
-
+	
 
 	
 }
